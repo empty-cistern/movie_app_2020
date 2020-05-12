b1f554e21d9de752c21d13a6ef4c3624ed857ead
subin-dev1012 <qlstnwh1012@naver.com>
2020-05-12 17:30:32 +0900

    Updates

---
+++ precache-manifest.bc1cc4caa740f9d5734d39540aae56fc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b239f87950510ff84a3db85ebc63bdb",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "242d5ea08728ad98b5ec",
+    "url": "/movie_app_2020/static/css/main.6e104ef1.chunk.css"
+  },
+  {
+    "revision": "261fc6083f1d5d248b8b",
+    "url": "/movie_app_2020/static/js/2.769f61a3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app_2020/static/js/2.769f61a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "242d5ea08728ad98b5ec",
+    "url": "/movie_app_2020/static/js/main.d86cf453.chunk.js"
+  },
+  {
+    "revision": "9927c9d5c16bc5ac0d63",
+    "url": "/movie_app_2020/static/js/runtime-main.72517e8e.js"
+  }
+]);
